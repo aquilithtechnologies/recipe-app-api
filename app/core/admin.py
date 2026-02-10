fadbc347cd39d3d4fe03b60d99eac9470f5669a7
ugochukwuemeka <ugochukwuemeka.inc@hotmail.com>
2026-02-10 13:21:46 +0100

    Fix Django admin customization and add user page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,5 @@
 """
-git status
+Django admin customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
@@ -7,10 +7,12 @@ from django.utils.translation import gettext_lazy as _
 
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
+
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         (
@@ -25,20 +27,25 @@ class UserAdmin(BaseUserAdmin):
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
-    readonly_fields = ['last_login']
+
+    readonly_fields = ('last_login',)
+
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': (
-                'email',
-                'password1',
-                'password2',
-                'name',
-                'is_active',
-                'is_staff',
-                'is_superuser',
-            )
-        }),
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'email',
+                    'password1',
+                    'password2',
+                    'name',
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                ),
+            },
+        ),
     )
 
 
