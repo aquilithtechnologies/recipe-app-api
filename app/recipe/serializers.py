309dc676577d26bbfd4a72ee5d5d776659ffed4e
ugochukwuemeka <ugochukwuemeka.inc@hotmail.com>
2026-06-19 18:05:32 +0100

    Add recipe API

---
+++ app/recipe/serializers.py
@@ -0,0 +1,17 @@
+"""
+Serializer for recipe app
+"""
+from rest_framework import serializers
+
+from core.models import Recipe
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serializer for recipe objects."""
+
+    class Meta:
+        model = Recipe
+        fields = [
+            'id', 'title', 'time_minutes', 'price', 'description', 'link',
+        ]
+        read_only_fields = ['id']
